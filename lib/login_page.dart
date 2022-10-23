4a57a5190117560ac49777eb5868206122c0af3c
Takuya Yamamoto <tkyymmt0x0@gmail.com>
2022-10-23 21:28:39 +0900

    commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,14 +9,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPage extends State<LoginPage> {
-  void _gotoProfilePage() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const ProfilePage()),
-    );
-  }
-
   bool _isObscure = true;
+  final _emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,73 +22,121 @@ class _LoginPage extends State<LoginPage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-                height: 80,
-                width: 400,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      borderSide: const BorderSide(
-                        color: Colors.blue,
-                        width: 2.0,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      borderSide: const BorderSide(
-                        color: Colors.blue,
-                        width: 2.0,
-                      ),
-                    ),
-                    labelText: 'ユーザー名',
-                  ),
-                )),
-            SizedBox(
-                height: 80,
-                width: 400,
-                child: TextFormField(
-                  obscureText: _isObscure,
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                          _isObscure ? Icons.visibility_off : Icons.visibility),
-                      onPressed: () {
-                        setState(() {
-                          _isObscure = !_isObscure;
-                        });
-                      },
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      borderSide: const BorderSide(
-                        color: Colors.blue,
-                        width: 2.0,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      borderSide: const BorderSide(
-                        color: Colors.blue,
-                        width: 2.0,
-                      ),
-                    ),
-                    labelText: 'パスワード',
-                  ),
-                )),
-            SizedBox(
-              height: 40,
-              child: ElevatedButton(
-                  onPressed: _gotoProfilePage,
-                  child: const Text(
-                    'ログイン',
-                    style: TextStyle(fontSize: 20),
-                  )),
-            )
-          ],
+          children: <Widget>[_emailForm(), _passwordForm(), _loginButton()],
         ),
       ),
+    );
+  }
+
+  Widget _emailForm() {
+    return SizedBox(
+        height: 80,
+        width: 400,
+        child: TextFormField(
+          //key: _formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          validator: (value) {
+            if (value == null || value.isEmpty || !value.contains('@'))
+              return 'メールアドレスを入力してください';
+            _emailFilled = true;
+            if (_passwordFilled && _emailFilled) setState(() {});
+            return null;
+          },
+          controller: _emailController,
+          decoration: InputDecoration(
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: const BorderSide(
+                color: Colors.blue,
+                width: 2.0,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: const BorderSide(
+                color: Colors.blue,
+                width: 2.0,
+              ),
+            ),
+            labelText: 'メールアドレス',
+          ),
+        ));
+  }
+
+  Widget _passwordForm() {
+    return SizedBox(
+        height: 80,
+        width: 400,
+        child: TextFormField(
+          /*
+          onChanged: (value) {
+            
+          },
+          */
+          //key: _formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          validator: (value) {
+            if (value == null || value.isEmpty)
+              return 'パスワードを入力してください';
+            else if (value.length < 8) return 'パスワードは8文字以上で入力してください';
+            _passwordFilled = true;
+            if (_passwordFilled && _emailFilled) setState(() {});
+            return null;
+          },
+          obscureText: _isObscure,
+          decoration: InputDecoration(
+            suffixIcon: IconButton(
+              icon: Icon(_isObscure ? Icons.visibility_off : Icons.visibility),
+              onPressed: () {
+                setState(() {
+                  _isObscure = !_isObscure;
+                });
+              },
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: const BorderSide(
+                color: Colors.blue,
+                width: 2.0,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: const BorderSide(
+                color: Colors.blue,
+                width: 2.0,
+              ),
+            ),
+            labelText: 'パスワード',
+          ),
+        ));
+  }
+
+  bool _emailFilled = false;
+  bool _passwordFilled = false;
+  Widget _loginButton() {
+    return SizedBox(
+      height: 40,
+      child: ElevatedButton(
+          onPressed: !_emailFilled || !_passwordFilled
+              ? null
+              : () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              ProfilePage(email: _emailController.text)));
+                  /*
+                    if (_formKey.currentState!.validate())
+                      _gotoProfilePage();
+                    else
+                      _formKey.currentState!.save();
+                      */
+                },
+          child: const Text(
+            'ログイン',
+            style: TextStyle(fontSize: 20),
+          )),
     );
   }
 }
